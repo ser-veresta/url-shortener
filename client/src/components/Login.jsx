ef1e39def82a6045e793d12ac69d11e68557985e
ser veresta <subramaniang573@gmail.com>
2021-09-06 16:43:29 +0530

    client deploy ready

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -94,7 +94,10 @@ const Login = () => {
     const { email, password } = values;
 
     try {
-      const { data } = await axios.post("http://localhost:5000/api/v1/auth/login", { email, password });
+      const { data } = await axios.post("https://url-shortener-02.herokuapp.com/api/v1/auth/login", {
+        email,
+        password,
+      });
 
       localStorage.setItem("token", data.token);
 
